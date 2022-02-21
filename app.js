a64d8a1cd806ea677cb30e1a9651b3c8db7d2041
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-02-22 00:16:27 +0530

    changing all the Files

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const routes = require('./routes');
-console.log(routes.someText);
-const server =  http.createServer(routes.handler);
-server.listen(8080);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(8080);
