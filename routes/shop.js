40891c1c403c921ace33b4c001f980f809eed61e
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-02-24 19:43:33 +0530

    changing to MVC

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,11 @@
 const path = require('path');
 
 const express = require('express');
+
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const rootDir = require('../util/path')
-
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir,'views','shop.html'));
-});
-
+router.get('/', productsController.getProducts);
 
 module.exports = router;
