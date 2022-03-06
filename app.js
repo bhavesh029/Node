ee79dfab195f8975228ef37212d597deafbd9c28
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-03-06 12:26:34 +0530

    updating product with sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { reset } = require('nodemon');
+//const { reset } = require('nodemon');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,5 +22,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync()
+.then(result => {
+    //console.log(result);
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+});
 
-app.listen(3000);
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(() => {
-    res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
   })
+  .then(result => console.log('Product Added'))
   .catch(err => console.log(err));
-  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -73,3 +74,4 @@ exports.postDeleteProduct = (req, res, next) => {
   Product.deleteById(prodId);
   res.redirect('/admin/products');
 }
+
